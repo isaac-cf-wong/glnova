a39dd416421a510e9e2cfc1ad5cad734715c2773
Isaac C. F. Wong <23655285+isaac-cf-wong@users.noreply.github.com>
2026-01-18 15:21:30 +0100

    chore: Remove deleted functions in the public API

--- src/glnova/__init__.py
+++ src/glnova/__init__.py
@@ -4,7 +4,6 @@ Top-level package for glnova.
 
 from __future__ import annotations
 
-from glnova.hello_world import goodbye_world, hello_goodbye, hello_world, say_goodbye, say_hello
 from glnova.version import __version__
 
-__all__ = ["__version__", "goodbye_world", "hello_goodbye", "hello_world", "say_goodbye", "say_hello"]
+__all__ = ["__version__"]
