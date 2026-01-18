3844122aff299182dd9c3959c4123ffae6e90b4d
Isaac C. F. Wong <23655285+isaac-cf-wong@users.noreply.github.com>
2026-01-18 16:32:47 +0100

    chore: Expose the following functions to the public API
    - `process_async_response_with_last_modified`
    - `process_response_with_last_modified`

--- src/glnova/utils/__init__.py
+++ src/glnova/utils/__init__.py
@@ -3,5 +3,11 @@
 from __future__ import annotations
 
 from glnova.utils.log import get_version_information, setup_logger
+from glnova.utils.response import process_async_response_with_last_modified, process_response_with_last_modified
 
-__all__ = ["get_version_information", "setup_logger"]
+__all__ = [
+    "get_version_information",
+    "process_async_response_with_last_modified",
+    "process_response_with_last_modified",
+    "setup_logger",
+]
