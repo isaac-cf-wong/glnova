bb41dec9c213ded3479b8a94c0e28b62128ae52f
Isaac C. F. Wong <23655285+isaac-cf-wong@users.noreply.github.com>
2026-01-18 16:19:16 +0100

    feat: Add `AsyncResource` for asynchronous GitLab API resources

---
+++ src/glnova/resource/async_resource.py
@@ -0,0 +1,81 @@
+"""Asynchronous Resource Base Class for GitLab API interactions."""
+
+from __future__ import annotations
+
+from typing import Any
+
+from aiohttp import ClientResponse
+
+from glnova.client.async_gitlab import AsyncGitLab
+
+
+class AsyncResource:
+    """Base class for asynchronous GitLab API resources."""
+
+    def __init__(self, client: AsyncGitLab) -> None:
+        """Initialize the Resource with a AsyncGitLab client.
+
+        Args:
+            client: An instance of the AsyncGitLab client.
+        """
+        self.client = client
+
+    async def _get(self, endpoint: str, **kwargs: Any) -> ClientResponse:
+        """Helper method to perform a GET request.
+
+        Args:
+            endpoint: The API endpoint.
+            **kwargs: Additional arguments for the request.
+
+        Returns:
+            The ClientResponse object.
+        """
+        return await self.client._request(method="GET", endpoint=endpoint, **kwargs)
+
+    async def _post(self, endpoint: str, **kwargs: Any) -> ClientResponse:
+        """Helper method to perform a POST request.
+
+        Args:
+            endpoint: The API endpoint.
+            **kwargs: Additional arguments for the request.
+
+        Returns:
+            The ClientResponse object.
+        """
+        return await self.client._request(method="POST", endpoint=endpoint, **kwargs)
+
+    async def _put(self, endpoint: str, **kwargs: Any) -> ClientResponse:
+        """Helper method to perform a PUT request.
+
+        Args:
+            endpoint: The API endpoint.
+            **kwargs: Additional arguments for the request.
+
+        Returns:
+            The ClientResponse object.
+        """
+        return await self.client._request(method="PUT", endpoint=endpoint, **kwargs)
+
+    async def _delete(self, endpoint: str, **kwargs: Any) -> ClientResponse:
+        """Helper method to perform a DELETE request.
+
+        Args:
+            endpoint: The API endpoint.
+            **kwargs: Additional arguments for the request.
+
+        Returns:
+            The ClientResponse object.
+        """
+        return await self.client._request(method="DELETE", endpoint=endpoint, **kwargs)
+
+    async def _patch(self, endpoint: str, **kwargs: Any) -> ClientResponse:
+        """Helper method to perform a PATCH request.
+
+        Args:
+            endpoint: The API endpoint.
+            **kwargs: Additional arguments for the request.
+
+        Returns:
+            The ClientResponse object.
+        """
+        return await self.client._request(method="PATCH", endpoint=endpoint, **kwargs)
