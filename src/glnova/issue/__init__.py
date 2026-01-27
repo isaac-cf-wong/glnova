0a52e1455da5636faa5dc8d56993565b8716e353
Isaac C. F. Wong <23655285+isaac-cf-wong@users.noreply.github.com>
2026-01-27 04:59:44 +0100

    chore: Expose `AsyncIssue` and `Issue` to the public API

--- src/glnova/issue/__init__.py
+++ src/glnova/issue/__init__.py
@@ -1,1 +1,8 @@
 """Issue module."""
+
+from __future__ import annotations
+
+from glnova.issue.async_issue import AsyncIssue
+from glnova.issue.issue import Issue
+
+__all__ = ["AsyncIssue", "Issue"]
