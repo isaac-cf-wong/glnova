1bd76a50bdb3b490a66613cac088854e940b9a79
Isaac C. F. Wong <23655285+isaac-cf-wong@users.noreply.github.com>
2026-01-31 18:48:41 +0100

    feat: Add `AccountConfig` and `Config` to define the configurations

---
+++ src/glnova/config/__init__.py
@@ -0,0 +1,7 @@
+"""Initialization of the configuration module for glnova."""
+
+from __future__ import annotations
+
+from glnova.config.model import AccountConfig, Config
+
+__all__ = ["AccountConfig", "Config"]
