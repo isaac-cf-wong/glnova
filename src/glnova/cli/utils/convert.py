7f7fb653ede1e2fc9e488ea2ac779384349b09c1
Isaac C. F. Wong <23655285+isaac-cf-wong@users.noreply.github.com>
2026-02-01 11:07:15 +0100

    feat: Add utility conversion functions to convert types

--- src/glnova/cli/utils/convert.py
+++ src/glnova/cli/utils/convert.py
@@ -3,7 +3,23 @@
 from __future__ import annotations
 
 
-def str_to_int(value: str | None) -> str | int | None:
+def str_to_int(value: str) -> int | str:
+    """Convert a string to an integer if possible.
+
+    Args:
+        value: The string to convert.
+
+    Returns:
+        The integer value if conversion is successful, otherwise the original string.
+
+    """
+    try:
+        return int(value)
+    except ValueError:
+        return value
+
+
+def str_to_int_or_none(value: str | None) -> str | int | None:
     """Convert a string to an integer if possible.
 
     Args:
@@ -15,13 +31,33 @@ def str_to_int(value: str | None) -> str | int | None:
     """
     if value is None:
         return value
-    try:
-        return int(value)
-    except ValueError:
-        return value
+    return str_to_int(value)
 
 
 def str_to_literal_or_int(
+    value: str,
+    literal_tuple: tuple[str, ...],
+) -> str | int:
+    """Convert a string to an integer or a literal.
+
+    Args:
+        value: The string to convert.
+        literal_tuple: A tuple of literal strings to check against.
+
+    Returns:
+        The integer value if conversion is successful, otherwise the original string if it matches a literal.
+
+    """
+    try:
+        return int(value)
+    except ValueError as e:
+        if value in literal_tuple:
+            return value
+        else:
+            raise ValueError(f"Value must be an integer or one of the literals: {literal_tuple}") from e
+
+
+def str_to_literal_or_int_or_none(
     value: str | None,
     literal_tuple: tuple[str, ...],
 ) -> str | int | None:
@@ -37,10 +73,4 @@ def str_to_literal_or_int(
     """
     if value is None:
         return value
-    try:
-        return int(value)
-    except ValueError as e:
-        if value in literal_tuple:
-            return value
-        else:
-            raise ValueError(f"Value must be an integer or one of the literals: {literal_tuple}") from e
+    return str_to_literal_or_int(value, literal_tuple)
