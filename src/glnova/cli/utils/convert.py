8722673bc4844c43d494e43133261eca104f91d8
Isaac C. F. Wong <23655285+isaac-cf-wong@users.noreply.github.com>
2026-02-01 09:03:09 +0100

    feat: Add utility functions to convert data types
    - Add `str_to_int`
    - Add `str_to_literal_or_int`

---
+++ src/glnova/cli/utils/convert.py
@@ -0,0 +1,46 @@
+"""Utility functions for converting data types."""
+
+from __future__ import annotations
+
+
+def str_to_int(value: str | None) -> str | int | None:
+    """Convert a string to an integer if possible.
+
+    Args:
+        value: The string to convert.
+
+    Returns:
+        The integer value if conversion is successful, otherwise the original string or None.
+
+    """
+    if value is None:
+        return value
+    try:
+        return int(value)
+    except ValueError:
+        return value
+
+
+def str_to_literal_or_int(
+    value: str | None,
+    literal_tuple: tuple[str, ...],
+) -> str | int | None:
+    """Convert a string to an integer, 'None', or 'Any' literal.
+
+    Args:
+        value: The string to convert.
+        literal_tuple: A tuple of literal strings to check against.
+
+    Returns:
+        The integer value if conversion is successful, the original string if it matches a literal, or None if the input is None.
+
+    """
+    if value is None:
+        return value
+    try:
+        return int(value)
+    except ValueError as e:
+        if value in literal_tuple:
+            return value
+        else:
+            raise ValueError(f"Value must be an integer or one of the literals: {literal_tuple}") from e
