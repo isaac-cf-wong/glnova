7de2f6d892a77a02c5d56f1ea0ed7bea9e958647
Isaac C. F. Wong <23655285+isaac-cf-wong@users.noreply.github.com>
2026-02-01 16:09:20 +0100

    feat: Add `Project` to `GitLab` client

--- src/glnova/client/gitlab.py
+++ src/glnova/client/gitlab.py
@@ -9,6 +9,7 @@ from requests import Response
 
 from glnova.client.base import Client
 from glnova.issue.issue import Issue
+from glnova.project.project import Project
 from glnova.user.user import User
 
 
@@ -28,6 +29,7 @@ class GitLab(Client):
 
         # Initialize resource handlers
         self.issue = Issue(client=self)
+        self.project = Project(client=self)
         self.user = User(client=self)
 
     def __str__(self) -> str:
